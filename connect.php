0d77630d350ff0c624ca461d6ce2fb7182ddc24c
vivek vadhiya <55307605+vivek992@users.noreply.github.com>
2020-10-29 18:23:04 +0530

    i uploaded signup form and connection with PHP

---
+++ connect.php
@@ -0,0 +1,44 @@
+<?php
+
+$server_name="localhost";
+$username= "root";
+$password="";
+$database_name="signup";
+
+
+// Create connection
+$conn=mysqli_connect($server_name, $username,$password,$database_name);
+
+
+
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+if(isset($_POST['save']))
+{
+    
+$Fname = $_POST['Fname'];
+$Lname= $_POST['Lname'];
+$Email = $_POST['Email'];
+$Username = $_POST['Username'];
+$Password = $_POST['Password'];
+
+
+//for inserting the values to mysql database
+
+$sql_query ="INSERT INTO user_info(Fname, Lname, Email, Username, Password)
+VALUES ('$Fname','$Lname','$Email','$Username','$Password')";
+
+if(mysqli_query($conn, $sql_query))
+{
+    echo "Registration Successfully....";
+}
+else
+{
+    echo "Error: " .$sql . "" . mysqli_error($conn);
+}
+mysqli_close($conn);
+}
+?>
